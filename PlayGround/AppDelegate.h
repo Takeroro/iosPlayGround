b332352c2870d82491302b0ed5a3591bfbce9403
Caotingjun <Caotingjun@terrencedemacbook-pro.local>
2016-10-28 14:33:29 +0800

    Initial Commit

---
+++ PlayGround/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayGround
+//
+//  Created by Caotingjun on 16/10/28.
+//  Copyright © 2016年 Caotingjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
