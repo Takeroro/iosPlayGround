1d0dcf0f497e0f32d086d482c1c92ed68b2e7f2a
Terrence <Caotingjun@TerrencedeMacBook-Pro.local>
2017-04-21 22:51:34 +0800

    fix rotate

--- PlayGround/TestRotateViewController.m
+++ PlayGround/TestRotateViewController.m
@@ -17,9 +17,15 @@ typedef enum : NSUInteger {
 @interface TestRotateViewController ()
 {
     rotateState _state;
+    
+    //这两个cgrect是同一个位置基于2个不同superView的值
     CGRect _orginFrame;
+    CGRect _rectInWindow;
+    
+    UIImageView *_myImageView;
 }
-@property (weak, nonatomic) IBOutlet UIImageView *myImageView;
+
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 
 @end
 
@@ -27,12 +33,8 @@ typedef enum : NSUInteger {
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-    UITapGestureRecognizer *smallTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleRotate)];
-    [_myImageView addGestureRecognizer:smallTap];
-    //记录原来的frame
-    //注意：一旦外部有对_myImageView的约束的话，以后压根不能操作他的frame
-    _orginFrame = _myImageView.frame;
+    
+    [self initMyImageView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -48,39 +50,65 @@ typedef enum : NSUInteger {
 
 #pragma mark - private
 
+- (void)initMyImageView
+{
+    _myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 180, 180)];
+    _myImageView.image = [UIImage imageNamed:@"guoguo"];
+    _myImageView.userInteractionEnabled = YES;
+    [_containerView addSubview:_myImageView];
+    
+    UITapGestureRecognizer *smallTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleRotate)];
+    [_myImageView addGestureRecognizer:smallTap];
+    //记录原来的frame
+    //注意：一旦外部有对_myImageView的约束的话，以后压根不能操作他的frame
+    _orginFrame = _myImageView.frame;
+}
+
+
 - (void)toggleRotate
 {
     if (_state == rotateState_rotated) {
-        [UIView animateWithDuration:0.8 animations:^{
-            _myImageView.transform = CGAffineTransformIdentity;//origial
-            //注意：一旦外部有对_myImageView的约束的话，以后压根不能操作他的frame
-            _myImageView.frame = _orginFrame;
-        } completion:^(BOOL finished) {
-            _state = rotateState_normal;
-            [_myImageView removeFromSuperview];
-            
-            [self.view addSubview:_myImageView];
-        }];
-        
+        [self exitFullScreen];
     } else {
-        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
-        [keyWindow addSubview:_myImageView];
-    
-        CGRect rectInWindow = [self.view convertRect:_myImageView.frame toView:keyWindow];
-        [_myImageView removeFromSuperview];
-        _myImageView.frame = rectInWindow;
-        [keyWindow addSubview:_myImageView];
-    
-        [UIView animateWithDuration:0.8 animations:^{
-            _myImageView.transform = CGAffineTransformMakeRotation(M_PI_2);
-            _myImageView.frame = CGRectMake(0, 0, DRScreenWidth, DRScreenHeight);
-            _myImageView.center = CGPointMake(DRScreenWidth/2, DRScreenHeight/2);
-        } completion:^(BOOL finished) {
-            _state = rotateState_rotated;
-        }];
+        [self enterFullScreen];
     }
 }
 
+- (void)exitFullScreen
+{
+    [UIView animateWithDuration:0.8 animations:^{
+        _myImageView.transform = CGAffineTransformIdentity;//origial
+        //注意：一旦外部有对_myImageView的约束的话，以后压根不能操作他的frame
+        _myImageView.frame = _rectInWindow;
+    } completion:^(BOOL finished) {
+        _state = rotateState_normal;
+        [_myImageView removeFromSuperview];
+        
+        [_containerView addSubview:_myImageView];
+        
+        _myImageView.frame = _orginFrame;
+    }];
+}
+
+- (void)enterFullScreen
+{
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+    [keyWindow addSubview:_myImageView];
+    
+    _rectInWindow = [_containerView convertRect:_myImageView.frame toView:keyWindow];
+    
+    [_myImageView removeFromSuperview];
+    _myImageView.frame = _rectInWindow;
+    [keyWindow addSubview:_myImageView];
+    
+    [UIView animateWithDuration:0.8 animations:^{
+        _myImageView.transform = CGAffineTransformMakeRotation(M_PI_2);
+        _myImageView.frame = CGRectMake(0, 0, DRScreenWidth, DRScreenHeight);
+        _myImageView.center = CGPointMake(DRScreenWidth/2, DRScreenHeight/2);
+    } completion:^(BOOL finished) {
+        _state = rotateState_rotated;
+    }];
+}
 
 /*
 #pragma mark - Navigation
